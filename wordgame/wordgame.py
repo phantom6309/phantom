e76ff96e2d8d143259c3c5b269409f5d342b88d9
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-03-21 12:42:06 +0300

    Update wordgame.py

--- wordgame/wordgame.py
+++ wordgame/wordgame.py
@@ -1,73 +1,50 @@
 # -*- coding: utf-8 -*-
 import discord
-from redbot.core import commands
-from redbot.core import Config
+from redbot.core import Config, commands
 
 class Profile(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=123456789)
+        self.config = Config.get_conf(self, identifier=1234567890)
 
-        # Set up default values for the config
-        default_global = {
-            "profiles": {}
-        }
-        self.config.register_global(**default_global)
-
-    @commands.command(name="profil oluştur")
-    async def create(self, ctx):
-        """Creates a profile for the user"""
-        author_id = str(ctx.author.id)
-
-        # Check if the user already has a profile
-        if author_id in self.config.profiles:
-            await ctx.send("You already have a profile!")
-            return
-
-        # Ask the user questions
-        questions = [
-            "What is your age?",
-            "What school do you attend?",
-            "What are your hobbies?",
-            "What is your favorite TV show?",
-            "What is your favorite movie?"
-        ]
-
+    @commands.command()
+    async def profil_oluştur(self, ctx):
+        """Asks user for profile information and saves it to config"""
+        questions = ["What is your age?", "What school do you attend?", "What are your hobbies?", "What is your favorite TV show?", "What is your favorite movie?"]
         answers = []
         for question in questions:
             await ctx.send(question)
-            response = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
-            answers.append(response.content)
+            try:
+                message = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=30.0)
+                answers.append(message.content)
+            except asyncio.TimeoutError:
+                await ctx.send("You took too long to answer, please try again later.")
+                return
+        user_id = str(ctx.author.id)
+        await self.config.user_from_id(user_id).age.set(answers[0])
+        await self.config.user_from_id(user_id).school.set(answers[1])
+        await self.config.user_from_id(user_id).hobbies.set(answers[2])
+        await self.config.user_from_id(user_id).favorite_tv_show.set(answers[3])
+        await self.config.user_from_id(user_id).favorite_movie.set(answers[4])
+        await ctx.send("Your profile has been created!")
 
-        # Save the profile to the config
-        profile_data = {
-            "age": answers[0],
-            "school": answers[1],
-            "hobbies": answers[2],
-            "favorite_tv_show": answers[3],
-            "favorite_movie": answers[4]
-        }
-        await self.config.profiles.set_raw(author_id, value=profile_data)
-        await ctx.send("Profile created!")
-
-    @commands.command(name="profil göster")
-    async def showprofile(self, ctx, member: discord.Member = None):
-        """Displays the specified user's profile"""
+    @commands.command()
+    async def profil_göster(self, ctx, member: discord.Member = None):
+        """Shows the profile information for a given user"""
         if not member:
             member = ctx.author
+        user_id = str(member.id)
+        age = await self.config.user_from_id(user_id).age()
+        school = await self.config.user_from_id(user_id).school()
+        hobbies = await self.config.user_from_id(user_id).hobbies()
+        favorite_tv_show = await self.config.user_from_id(user_id).favorite_tv_show()
+        favorite_movie = await self.config.user_from_id(user_id).favorite_movie()
 
-        profile_data = await self.config.profiles.get_raw(str(member.id))
-
-        if not profile_data:
-            await ctx.send(f"{member.display_name} does not have a profile.")
-            return
-
-        # Create the embed
-        embed = discord.Embed(title=f"{member.display_name}'s Profile")
-        embed.add_field(name="Age", value=profile_data["age"], inline=False)
-        embed.add_field(name="School", value=profile_data["school"], inline=False)
-        embed.add_field(name="Hobbies", value=profile_data["hobbies"], inline=False)
-        embed.add_field(name="Favorite TV Show", value=profile_data["favorite_tv_show"], inline=False)
-        embed.add_field(name="Favorite Movie", value=profile_data["favorite_movie"], inline=False)
+        embed = discord.Embed(title=f"{member.name}'s profile")
+        embed.add_field(name="Age", value=age, inline=False)
+        embed.add_field(name="School", value=school, inline=False)
+        embed.add_field(name="Hobbies", value=hobbies, inline=False)
+        embed.add_field(name="Favorite TV Show", value=favorite_tv_show, inline=False)
+        embed.add_field(name="Favorite Movie", value=favorite_movie, inline=False)
 
         await ctx.send(embed=embed)
