921b5a453b2cdfdacf955f0d426a5118240aacd1
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-03-21 16:15:33 +0300

    Update wordgame.py

--- wordgame/wordgame.py
+++ wordgame/wordgame.py
@@ -68,7 +68,7 @@ class Profile(commands.Cog):
     async def _göster(self, ctx, member: discord.Member):
      if member is None:
         # If no member is specified, use the author of the message as the member
-       member = ctx.author
+        member = ctx.author
         age = await self.config.member(member).age()
         school = await self.config.member(member).school()
         hobbies = await self.config.member(member).hobbies()
