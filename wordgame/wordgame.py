3d100b83cb64398fc56f84c32f905ee52a03059d
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-03-21 13:16:50 +0300

    Update wordgame.py

--- wordgame/wordgame.py
+++ wordgame/wordgame.py
@@ -1,64 +1,53 @@
 # -*- coding: utf-8 -*-
 import discord
-from datetime import datetime
-from redbot.core import commands, Config
-from typing import Optional
+from redbot.core import commands
+from redbot.core import Config
 
 class Profile(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=6547894321)
-
+        self.config = Config.get_conf(self, identifier=7895645342)
+        self.config.register_member(age=None, school=None, hobbies=None, favorite_tv_show=None, favorite_movie=None)
+    
     @commands.command()
-    async def profil(self, ctx, member: Optional[discord.Member] = None):
-        """Creates or shows user profile."""
-        if member is None:
-            member = ctx.author
-
-        user_data = await self.config.user(member).all()
-        if not user_data:
-            await self.create_profile(member)
-            user_data = await self.config.user(member).all()
-
-        embed = discord.Embed(title=f"{member.name}'s profile", color=member.color)
+    async def profil_oluştur(self, ctx):
+        await ctx.author.send("Merhaba, profilinizi oluşturmak için birkaç soru sormak istiyoruz. Lütfen yaşınızı söyler misiniz?")
+        age = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        
+        await ctx.author.send("Hangi okulda okuyorsunuz?")
+        school = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        
+        await ctx.author.send("Hangi hobileriniz var?")
+        hobbies = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        
+        await ctx.author.send("En sevdiğiniz TV programı nedir?")
+        favorite_tv_show = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        
+        await ctx.author.send("En sevdiğiniz film nedir?")
+        favorite_movie = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        
+        await self.config.member(ctx.author).age.set(age.content)
+        await self.config.member(ctx.author).school.set(school.content)
+        await self.config.member(ctx.author).hobbies.set(hobbies.content)
+        await self.config.member(ctx.author).favorite_tv_show.set(favorite_tv_show.content)
+        await self.config.member(ctx.author).favorite_movie.set(favorite_movie.content)
+        
+        await ctx.send("Profiliniz başarıyla oluşturuldu!")
+    
+    @commands.command()
+    async def profil_göster(self, ctx, member: discord.Member):
+        age = await self.config.member(member).age()
+        school = await self.config.member(member).school()
+        hobbies = await self.config.member(member).hobbies()
+        favorite_tv_show = await self.config.member(member).favorite_tv_show()
+        favorite_movie = await self.config.member(member).favorite_movie()
+        
+        embed = discord.Embed(title=f"{member.display_name}'s Profile", color=0x00ff00)
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=f"ID: {member.id}")
-        fields = [("Age", user_data.get("age", "N/A")),
-                  ("School", user_data.get("school", "N/A")),
-                  ("Hobbies", user_data.get("hobbies", "N/A")),
-                  ("Favorite TV show", user_data.get("favorite_tv_show", "N/A")),
-                  ("Favorite movie", user_data.get("favorite_movie", "N/A")),
-                  ("Role", user_data.get("role", "N/A")),
-                  ("Time in server", self.time_in_server(member))]
-
-        for name, value in fields:
-            if value != "pas":
-                embed.add_field(name=name, value=value, inline=False)
-
+        embed.add_field(name="Yaş", value=age or "Bilinmiyor")
+        embed.add_field(name="Okul", value=school or "Bilinmiyor")
+        embed.add_field(name="Hobiler", value=hobbies or "Bilinmiyor")
+        embed.add_field(name="En Sevdiğiniz TV Programı", value=favorite_tv_show or "Bilinmiyor")
+        embed.add_field(name="En Sevdiğiniz Film", value=favorite_movie or "Bilinmiyor")
+        
         await ctx.send(embed=embed)
-
-    @commands.command()
-    async def create_profile(self, ctx, member):
-        """Asks the user questions to create a profile."""
-        questions = {"age": "What's your age?",
-                     "school": "What school do you go to?",
-                     "hobbies": "What are your hobbies?",
-                     "favorite_tv_show": "What's your favorite TV show?",
-                     "favorite_movie": "What's your favorite movie?",
-                     "role": "What's your role in the server? (ex: Moderator, Admin, etc.)",
-                     "time_in_server": self.time_in_server(member)}
-        data = {}
-        for key, question in questions.items():
-            if key == "time_in_server":
-                data[key] = question
-            else:
-                await member.send(question)
-                response = await self.bot.wait_for("message", check=lambda m: m.author == member)
-                if response.content.lower() != "pas":
-                    data[key] = response.content
-        await self.config.user(member).set(data)
-
-    def time_in_server(self, member):
-        time_in_server = datetime.now() - member.joined_at
-        return str(time_in_server).split('.')[0]
-
